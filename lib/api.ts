99d46940e835a8faba8b53cc25a2813ca36e908c
Bruno De Masi <brunodemasi1@gmail.com>
2020-11-20 11:01:31 -0300

    fix: api url fix

--- lib/api.ts
+++ lib/api.ts
@@ -3,11 +3,15 @@ import axios from 'axios';
 import { IApiOptions } from './types';
 
 export const requestHandler = async ({ method, data, path }: IApiOptions) => {
-  const url = `http://localhost:8080/listeners${path ? '/' + path : ''}`;
+  try {
+    const url = `http://localhost:8080/listeners${path ? '/' + path : ''}`;
 
-  await axios({
-    method,
-    url,
-    data,
-  })
+    await axios({
+      method,
+      url,
+      data,
+    })
+  } catch (err) {
+    console.log('Error when sending bug to Buggy')
+  }
 }
