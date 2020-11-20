972405d648eb7cd57ceb1fb419557e26ccf023ed
Bruno De Masi <brunodemasi1@gmail.com>
2020-11-20 11:01:11 -0300

    feat: handlers can have options now and look for already instanceof Error

--- lib/handlers.ts
+++ lib/handlers.ts
@@ -1,34 +1,62 @@
-import { Response, NextFunction } from 'express';
 import { parseStack } from './parsers';
 import { requestHandler } from './api';
+import { MiddlewareError } from './types';
 
-export const requestError = async (error: Error, _: any, response: Response, next: NextFunction): Promise<void> => {
-  if (!error) {
-    next();
-  }
-
-  const listenerKey = global.__DSN_STRING__;
-
-  if (!listenerKey || listenerKey === '') {
-    next();
-  }
-
-  const { name, message } = error;
-
-  const { line, where } = parseStack(error);
-
-  await requestHandler({
-    method: 'POST',
-    path: 'express',
-    data: {
-      name,
-      message,
-      stack_where: where,
-      stack_line: line,
-      listener_key: listenerKey,
-      type: 'backend'
+export function requestError(options?: {
+  notInstanceOf?: any,
+  statusCode?: number;
+}): (
+  error: MiddlewareError,
+  _: any,
+  response: Response,
+  next: (error: MiddlewareError) => void,
+) => void {
+  return function buggyErrorMiddleware(
+    error: MiddlewareError,
+    _: any,
+    __: any,
+    next: (error: MiddlewareError) => void
+  ): void {
+    if (!error) {
+      next(error);
     }
-  })
 
-  next();
+    if (options?.notInstanceOf && error instanceof options.notInstanceOf
+      && (
+        error.status !== 500 ||
+        error.statusCode !== 500 ||
+        error.status_code !== 500
+      )
+    ) {
+      next(error);
+      return;
+    }
+
+    const listenerKey = global.__DSN_STRING__;
+
+    if (!listenerKey || listenerKey === '') {
+      next(error);
+      return;
+    }
+
+    const { name, message, query } = error;
+
+    const { line, where } = parseStack(error);
+
+    requestHandler({
+      method: 'POST',
+      path: 'express',
+      data: {
+        name,
+        message,
+        stack_where: where,
+        stack_line: line,
+        listener_key: listenerKey,
+        query,
+        type: 'backend'
+      }
+    }).then(() => {});
+
+    next(error);
+  }
 }
